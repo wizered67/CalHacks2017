6f45e45742b0e414506b69af247c796a84ac2008
Sabrina Wong <sabrina.wong@berkeley.edu>
2017-10-07 03:09:14 -0700

    nutrients added

--- src/main/java/CreateDB.sql
+++ src/main/java/CreateDB.sql
@@ -37,4 +37,37 @@ CREATE TABLE DailyIntakes (
     PRIMARY KEY(nutrientId, lowerAge, upperAge, sex)
 );
 
--- INSERT INTO DailyIntakes VALUES (301, 5, 8, "M", 5);
+INSERT INTO DailyIntakes VALUES (301, 9, 18, "M", 1300);
+INSERT INTO DailyIntakes VALUES (301, 9, 18, "F", 1300);
+INSERT INTO DailyIntakes VALUES (301, 19, 70, "M", 1000);
+INSERT INTO DailyIntakes VALUES (301, 19, 50, "F", 1000);
+INSERT INTO DailyIntakes VALUES (301, 51, 300, "F", 1200);
+INSERT INTO DailyIntakes VALUES (301, 71, 300, "M", 1200);
+
+INSERT INTO DailyIntakes VALUES (203, 9, 13, "M", 34);
+INSERT INTO DailyIntakes VALUES (203, 14, 18, "M", 52);
+INSERT INTO DailyIntakes VALUES (203, 19, 300, "M", 56);
+INSERT INTO DailyIntakes VALUES (203, 9, 13, "F", 34);
+INSERT INTO DailyIntakes VALUES (203, 14, 300, "F", 46);
+
+INSERT INTO DailyIntakes VALUES (320, 9, 13, "M", 600);
+INSERT INTO DailyIntakes VALUES (203, 14, 300, "M", 900);
+INSERT INTO DailyIntakes VALUES (203, 9, 13, "F", 600);
+INSERT INTO DailyIntakes VALUES (203, 14, 300, "F", 700);
+
+INSERT INTO DailyIntakes VALUES (401, 9, 13, "M", 45);
+INSERT INTO DailyIntakes VALUES (401, 9, 13, "F", 45);
+INSERT INTO DailyIntakes VALUES (401, 14, 18, "M", 75);
+INSERT INTO DailyIntakes VALUES (401, 14, 18, "F", 65);
+INSERT INTO DailyIntakes VALUES (401, 19, 300, "M", 90);
+INSERT INTO DailyIntakes VALUES (401, 19, 300, "F", 75);
+
+INSERT INTO DailyIntakes VALUES (324, 9, 70, "M", 15);
+INSERT INTO DailyIntakes VALUES (324, 9, 70, "F", 15);
+INSERT INTO DailyIntakes VALUES (324, 71, 300, "M", 20);
+INSERT INTO DailyIntakes VALUES (324, 71, 300, "F", 20);
+
+INSERT INTO DailyIntakes VALUES (323, 9, 13, "M", 11);
+INSERT INTO DailyIntakes VALUES (323, 9, 13, "F", 11);
+INSERT INTO DailyIntakes VALUES (323, 14, 300, "M", 15);
+INSERT INTO DailyIntakes VALUES (323, 14, 300, "F", 15);
